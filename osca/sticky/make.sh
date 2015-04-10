c50d44bff05a5346f16f4141928b21273fad5885
calint <calin.tenitchi@gmail.com>
2015-04-10 02:12:58 -0500

    .

--- osca/sticky/make.sh
+++ osca/sticky/make.sh
@@ -1,4 +1,15 @@
-#!/bin/sh
-app=sticky
-gcc -o $app $app.c -Wfatal-errors -Wall -Wextra `pkg-config --cflags --libs gtk+-3.0 gtksourceview-3.0`&&
-ls -l $app
+BIN=sticky
+SRC=sticky.c
+#OPTS=-Os
+WARNINGS="-Wall -Wextra"
+LIBS=$(pkg-config --cflags --libs gtk+-3.0 gtksourceview-3.0)
+
+echo &&
+gcc  -o $BIN  $SRC $LIBS $OPTS $WARNINGS && 
+echo    "             lines   words  chars" &&
+echo -n "       wc:" &&
+cat $SRC|wc
+echo -n "wc zipped:" &&
+cat $SRC|gzip|wc &&
+echo && ls -o --color $BIN &&
+echo
