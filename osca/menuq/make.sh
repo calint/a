84182ebbbf1f827127ef33efa5f99b2360fa6d29
calint <calin.tenitchi@gmail.com>
2015-04-13 17:41:04 -0500

    .

--- osca/menuq/make.sh
+++ osca/menuq/make.sh
@@ -1,15 +1,19 @@
+CC="gcc -std=c11"
 BIN=menuq
-SRC=menuq.c
+SRC=src/menuq.c
 #OPTS=-Os
 WARNINGS="-Wall -Wextra"
 LIBS=-lX11
 
 echo &&
-gcc  -o $BIN  $SRC $LIBS $OPTS $WARNINGS && 
+$CC  -o $BIN  $SRC $LIBS $OPTS $WARNINGS && 
 echo    "             lines   words  chars" &&
-echo -n "       wc:" &&
+echo -n "   source:" &&
 cat $SRC|wc
 echo -n "wc zipped:" &&
 cat $SRC|gzip|wc &&
 echo && ls -o --color $BIN &&
-echo
+echo &&
+valgrind --leak-check=yes --leak-check=full --show-leak-kinds=all ./$BIN
+#valgrind --leak-check=yes --leak-check=full ./$BIN
+#valgrind --leak-check=yes ./$BIN
