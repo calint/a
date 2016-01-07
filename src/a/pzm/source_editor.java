95fce3812d7fb39d6115b5d052e6509511255fa3
calin <calin@192.168.1.181>
2016-01-07 19:29:03 +0700

    .

--- src/a/pzm/source_editor.java
+++ src/a/pzm/source_editor.java
@@ -13,14 +13,12 @@ import b.a;
 import b.req;
 import b.xwriter;
 final public class source_editor extends a{
-	int focusline;
-	private int lstfocusline;
-	//	public a brkpts;
-	private Set<Integer> brkptsset=new HashSet<Integer>();
 	public a src;
 	//	public a resrc;
 	public a sts;
 	public line_numbers ln;
+	public boolean ommit_compiling_source_from_disassembler=false;
+	private statement code;
 	public void to(final xwriter x) throws Throwable{
 		x.style("def","font-weight:bold");//a name
 //		x.style("fc","font-style: italic");//function name refered
@@ -39,26 +37,9 @@ final public class source_editor extends a{
 		x.focus(src);
 		//		x.td().spaned(resrc);
 		x.table_();
-		if(code!=null)
-				code.to(x);
+//		if(code!=null)
+//				code.to(x);
 	}
-	public boolean isonbrkpt(final int srclno){
-		return brkptsset.contains(srclno);
-	}
-	//	synchronized public void x_brk(xwriter x,String s)throws Throwable{
-	//		final int lno=Integer.parseInt(s);
-	//		if(brkptsset.contains(lno)){
-	//			brkptsset.remove(lno);
-	//			brkpts.set(brkptsset.toString());
-	//			x.pl("var e=$('"+id()+"').getElementsByTagName('ol')[0].getElementsByTagName('li')["+(lno-1)+"];e.className=e._oldcls;");
-	//			return;
-	//		}
-	//		brkptsset.add(lno);
-	//		brkpts.set(brkptsset.toString());
-	//		x.pl("var e=$('"+id()+"').getElementsByTagName('ol')[0].getElementsByTagName('li')["+(lno-1)+"];e._oldcls=e.className;if(!e._oldcls)e._oldcls='';e.className='brk';");
-	//	}
-	public final static boolean ommit_compiling_source_from_disassembler=false;
-	statement code;
 	synchronized public void x_f3(xwriter x,String s) throws Throwable{
 		final String source="{"+src.str()+"}";
 		final reader r=new reader(new StringReader(source));
@@ -81,20 +62,16 @@ final public class source_editor extends a{
 			code.binary_to(b);
 			b.link();
 			final int[]new_rom=new int[b.ix()];
-			System.arraycopy(rom, 0, new_rom, 0, b.ix());
+			System.arraycopy(rom,0,new_rom,0,b.ix());
 			final int nregs_aft=b.registers_available.size();
 			if(nregs_pre!=nregs_aft)
 				throw new Error("available register count pre binary_to and after do not match: pre="+nregs_pre+"  after="+nregs_aft+"   "+b.registers_available);
 			pl("registers available "+b.registers_available.size()+" "+b.registers_available);
-			pl("*** toc");
-			p.toc.entrySet().forEach(me->{
-				pl(me.getKey());
-			});
+//			pl("*** toc");
+//			p.toc.entrySet().forEach(me->{
+//				pl(me.getKey());
+//			});
 			pl("*** done");
-
-//			final xwriter src=new xwriter();
-//			code.source_to(src);
-//			resrc.set(src.toString());
 			if(x==null) return;
 			x.xu(sts.clr(),code);
 			//			el.source_to(x.xub(resrc,true,true));x.xube();
@@ -118,41 +95,6 @@ final public class source_editor extends a{
 			x.xu(sts.set("line "+r.location_in_source()+": "+t.getMessage()));
 			//			x.xalert(t.getMessage());
 		}
-		//		final program p;
-		//		try{
-		//			p=new program(src.str());
-		//			p.build();
-		//			sts.set(p.program_length+" ");
-		//			final int prev_focus_line=ln.focus_line;
-		//			ln.focus_line=0;
-		//			if(x!=null){
-		//				if(prev_focus_line!=ln.focus_line) x.xu(ln);
-		//				x.xu(sts);
-		//			}
-		//		}catch(Throwable t){
-		//			log(t);
-		//			sts.set(t.toString()+"\n");
-		//			if(t instanceof compiler_error) ln.focus_line=((compiler_error)t).source_location_line();
-		//			if(x!=null) x.xu(ln).xu(sts);
-		//			return;
-		//		}
-		//		if(!ommit_compiling_source_from_disassembler){
-		//			try{
-		//				final program p2=new program(p.toString());
-		//				p2.build();
-		//				if(!p.is_binary_equal(p2)) throw new Error("not binary equivalent");
-		//			}catch(Throwable t){
-		//				log(t);
-		//				if(x!=null) x.xalert("reverse compilation failed: "+t.toString());
-		//				return;
-		//			}
-		//		}
-		//		ev(x,this,p);
-	}
-	public void xfocusline(xwriter x){
-		if(lstfocusline!=0) x.pl("var e=$('"+id()+"').getElementsByTagName('ol')[0].getElementsByTagName('li')["+lstfocusline+"];e.className=e._oldcls;");
-		x.pl("var e=$('"+id()+"').getElementsByTagName('ol')[0].getElementsByTagName('li')["+(focusline-1)+"];e._oldcls=e.className;e.className='stp';");
-		lstfocusline=focusline;
 	}
 	private static final long serialVersionUID=11;
 }
