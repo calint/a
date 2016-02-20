79ead09caadec6164bfd4739f8d1fa8b16f36e11
calint <calin.tenitchi@gmail.com>
2016-02-20 04:57:08 -0600

    .

--- osca/sh/xii-commit-all-projects-in-workspace-to-github.sh
+++ osca/sh/xii-commit-all-projects-in-workspace-to-github.sh
@@ -1,1 +1,1 @@
-for d in $(ls $WORKSPACE);do echo "*** $d" ; cd $WORKSPACE/$d ; git remote set-url origin git@github.com:calint/$d.git ; git add . ; git commit -m "." ; git push --all ; git push --tags; cd ..;done;
+for d in $(ls $WORKSPACE);do echo "*** $d" ; cd $WORKSPACE/$d ; git remote set-url origin git@github.com:calint/$d.git ; git add . ; git commit -m "." ; git push --all ; git push --tags; cd ..; done;
