0acedcb0992d12282ae61ebd11bbe8bd7ca35501
calint <calin.tenitchi@gmail.com>
2016-02-20 04:52:07 -0600

    .

---
+++ osca/make-etc-skel-from-home.sh
@@ -0,0 +1,18 @@
+SKEL=$OSCA/etc/skel
+echo "     source: $HOME"
+echo destination: $SKEL
+
+rm -rf $SKEL&&
+mkdir $SKEL&&
+cp -av $HOME/.alias $SKEL&&
+cp -av $HOME/.bashrc $SKEL&&
+cp -av $HOME/.gtkrc-2.0 $SKEL&&
+cp -av $HOME/.xsession $SKEL&&
+mkdir $SKEL/.config&&
+cp -rav $HOME/.config/leafpad $SKEL/.config&&
+cp -rav $HOME/.config/lxterminal $SKEL/.config&&
+cp -rav $HOME/.config/Thunar $SKEL/.config&&
+cp -rav $HOME/.config/vlc $SKEL/.config&&
+
+echo done
+
