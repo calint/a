3e828057aa3f14340afc311e27ab182456dfc2cd
calint <calin.tenitchi@gmail.com>
2017-06-13 21:22:49 +0700

    .

--- ba.sh
+++ ba.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 DIR=$(dirname "$0")
 cd $DIR &&
-cmd="java -cp bin`ls lib|while read f;do echo -n :;echo -n lib/$f;done` b.b $*" &&
+
+cmd="java -cp bin`ls $DIR/lib|while read f;do echo -n :;echo -n $DIR/lib/$f;done` b.b $*" &&
 echo \> $cmd;$cmd
 
 
--- cloud/linode/linode-install.sh
+++ cloud/linode/linode-install.sh
@@ -1,9 +1,41 @@
-[ ] create linode
-[ ] ssh-copy-id
-[ ] apt-get install openjre8
-[ ] scp install image
-[ ] unzip
-[ ] scp service file
-[ ] systemctl enable ba
-[ ] reboot
-[ ] confirm
+#!/bin/sh
+IP=$1
+IMG=~/a.zip
+
+echo " * init ubuntu 16 lts linode at $IP" &&
+echo " * copy ssh public key to root" &&
+ssh-copy-id root@$IP &&
+
+echo " * copy systemd service file ba.service to /etc/systemd/system/" &&
+scp ba.service root@$IP:/etc/systemd/system/ &&
+ssh root@$IP systemctl enable ba &&
+
+echo " * update package list" &&
+ssh root@$IP apt-get update &&
+
+echo " * update packages" &&
+ssh root@$IP apt-get -y upgrade &&
+
+echo " * install java runtime" &&
+ssh root@$IP apt-get -y install openjdk-8-jre-headless unzip &&
+
+echo " * copy install image  $IMG to $IP:/" &&
+#scp $IMG root@$IP:/
+
+echo " * unzip image" &&
+ssh root@$IP unzip /a.zip -d / && 
+
+echo " * adjust installed image" &&
+ssh root@$IP chmod +x /a/ba.sh && 
+
+echo " * reboot" &&
+ssh root@$IP reboot && 
+
+echo " * done"
+#[ ] apt-get install openjre8
+#[ ] scp install image
+#[ ] unzip
+#[ ] scp service file
+#[ ] systemctl enable ba
+#[ ] reboot
+#[ ] confirm
